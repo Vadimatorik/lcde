b45d87a6f5f889bc636aa5eaa3be700b8aa235d5
Vadimatorik <Deryabkinda@mail.ru>
2019-12-28 17:03:48 +0700

    Больший тестовый отступ. И тестовая заливка точками.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@
 int main (int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    mono_lcd *lcd = new mono_lcd(Qt::GlobalColor::green, Qt::GlobalColor::black, 128, 64, 0.05);
+    mono_lcd *lcd = new mono_lcd(Qt::GlobalColor::green, Qt::GlobalColor::black, 128, 64, 0.1);
+
+    for (uint8_t i = 0; i < 64; i++) {
+        lcd->set_point(i, i);
+    }
 
     lcd->setMinimumWidth(128 * 3);
     lcd->setMinimumHeight(64 * 3);
